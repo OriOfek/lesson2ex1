950361d578502db2396db6fc8ca211c07bde93a2
liemb <lb6031@bs.amalnet.k12.il>
2020-02-18 09:38:15 +0200

    seciond

--- app/src/main/java/com/example/lesson2ex1/MainActivity.java
+++ app/src/main/java/com/example/lesson2ex1/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.lesson2ex1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    int i;
+    ImageView iv;
+    Random rnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn=(Button) findViewById(R.id.btn);
+        iv=(ImageView) findViewById(R.id.imageView);
+        i=0;
+        rnd=new Random();
+    }
+
+    public void change(View view) {
+        i=rnd.nextInt(3);
+        btn.setText(String.valueOf(i));
+        switch (i)
+        {
+            case 1:
+
+                break;
+
+        }
+        }
     }
 }
